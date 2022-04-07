71396988f4171d455626616fb51030aabef62f57
Stine Hofstad Thronæs <s_t_i_n_e_93@hotmail.com>
2022-04-07 22:19:47 +0200

    la til så siden ikke resetter seg ved klikk, og at meldingen kun vises ved klikk på knappen

--- public/app.js
+++ public/app.js
@@ -2,11 +2,16 @@
 
 
 const input =document.querySelector('.add');
+const output=document.querySelector('.output');
+const knapp =document.querySelector('.knapp');
 
-input.addEventListener('click', () => {
+knapp.addEventListener('click', (e) => {
     const visMelding=firebase.functions().httpsCallable('visMelding');
+    e.preventDefault()
     visMelding({ melding: input.melding.value}).then(result => {
+       
         console.log(result.data);
+        input.reset()
       
       });
     }
